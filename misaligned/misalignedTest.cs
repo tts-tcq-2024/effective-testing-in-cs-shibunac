3c02e81e9cb9eeeea47630fd20a94023f9c99003
shibunac <shibunachedakal@gmail.com>
2024-11-04 16:02:30 +0530

    Update misalignedTest.cs

--- misaligned/misalignedTest.cs
+++ misaligned/misalignedTest.cs
@@ -1,56 +1,60 @@
-using NUnit.Framework;
+using System.Diagnostics;
 
-namespace MisalignedSpace.Tests
+namespace MisalignedSpace
 {
-    public class MisalignedTests
+    class ExcpectedStrings
     {
-        [Test]
-        public void TestPrintColorMapOutput()
+        Misaligned TestPrint = new Misaligned();
+        // Validate color mappings
+        static string[] expectedLines = {
+                    "0 | White | Blue",
+                    "1 | White | Orange",
+                    "2 | White | Green",
+                    "3 | White | Brown",
+                    "4 | White | Slate",
+                    "5 | Red | Blue",
+                    "6 | Red | Orange",
+                    "7 | Red | Green",
+                    "8 | Red | Brown",
+                    "9 | Red | Slate",
+                    "10 | Black | Blue",
+                    "11 | Black | Orange",
+                    "12 | Black | Green",
+                    "13 | Black | Brown",
+                    "14 | Black | Slate",
+                    "15 | Yellow | Blue",
+                    "16 | Yellow | Orange",
+                    "17 | Yellow | Green",
+                    "18 | Yellow | Brown",
+                    "19 | Yellow | Slate",
+                    "20 | Violet | Blue",
+                    "21 | Violet | Orange",
+                    "22 | Violet | Green",
+                    "23 | Violet | Brown",
+                    "24 | Violet | Slate"
+                };
+
+        public static void TestPrintColorMapOutput()
         {
-            // Arrange
-            var expectedOutput = 
-                "0 | White | Blue\n" +
-                "1 | White | Orange\n" +
-                "2 | White | Green\n" +
-                "3 | White | Brown\n" +
-                "4 | White | Slate\n" +
-                "5 | Red | Blue\n" +
-                "6 | Red | Orange\n" +
-                "7 | Red | Green\n" +
-                "8 | Red | Brown\n" +
-                "9 | Red | Slate\n" +
-                "10 | Black | Blue\n" +
-                "11 | Black | Orange\n" +
-                "12 | Black | Green\n" +
-                "13 | Black | Brown\n" +
-                "14 | Black | Slate\n" +
-                "15 | Yellow | Blue\n" +
-                "16 | Yellow | Orange\n" +
-                "17 | Yellow | Green\n" +
-                "18 | Yellow | Brown\n" +
-                "19 | Yellow | Slate\n" +
-                "20 | Violet | Blue\n" +
-                "21 | Violet | Orange\n" +
-                "22 | Violet | Green\n" +
-                "23 | Violet | Brown\n" +
-                "24 | Violet | Slate";
+            // Redirect Console output to a string
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                int result = Misaligned.printColorMap();
+                string output = sw.ToString();
 
-            // Act
-            string actualOutput = Misaligned.PrintColorMap();
+                // Check the total count of printed lines
+                Debug.Assert(result == 25, "Expected 25 lines of output.");
 
-            // Assert
-            Assert.AreEqual(expectedOutput.Trim(), actualOutput);
-        }
+                // Split the output into lines
+                var actualLines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
-        [Test]
-        public void TestPrintColorMapLineCount()
-        {
-            // Act
-            string result = Misaligned.PrintColorMap();
-            int lineCount = result.Split('\n').Length;
-
-            // Assert
-            Assert.AreEqual(25, lineCount);
+                // Validate each expected line against actual output
+                for (int i = 0; i < expectedLines.Length; i++)
+                {
+                    Debug.Assert(actualLines[i] == expectedLines[i], $"Expected '{expectedLines[i]}', but got '{actualLines[i]}'.");
+                }
+            }
         }
     }
 }
