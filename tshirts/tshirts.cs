c4a6cb7224941260b85faf306b83ad0b2cf6a5a2
shibunac <shibunachedakal@gmail.com>
2024-11-04 16:37:38 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -13,4 +13,14 @@ namespace TshirtSpace {
             }
         }
     }
+
+ protected virtual void Main(string[] args)
+ {
+   Debug.Assert(Size(37) == "S");
+   Debug.Assert(Size(40) == "M");
+   Debug.Assert(Size(43) == "L");
+   Debug.Assert(Size(38) == "M"); // Medium (new test)
+   Debug.Assert(Size(42) == "L"); // Medium (edge case)
+   Console.WriteLine("Test failure indicates bug in the implementation!");
+ }
 }
