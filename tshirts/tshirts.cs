1059debffb352f576afb5925784e4fe53cce2f51
shibunac <shibunachedakal@gmail.com>
2024-11-04 16:45:15 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -19,7 +19,6 @@ namespace TshirtSpace {
    Debug.Assert(Size(37) == "S");
    Debug.Assert(Size(40) == "M");
    Debug.Assert(Size(43) == "L");
-   Debug.Assert(Size(30) == "L"); // should fail
    Debug.Assert(Size(38) == "M"); // Medium (new test)
    Debug.Assert(Size(42) == "L"); // Medium (edge case)
    Console.WriteLine("Test failure indicates bug in the implementation!");
